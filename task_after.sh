433ef41ac9f82312246dea3e8b588043cfdaf6cf
Ethan <64431009+EthanWDC@users.noreply.github.com>
2022-07-13 23:09:12 +0800

    Update task_after.sh

--- task_after.sh
+++ task_after.sh
@@ -4,7 +4,7 @@ set -e
 jdpath="../JDHelloWorld_jd_scripts_main/"
 
 echo "overwrite sendNotify.js..."
-cat ./custom_sendNotify.js > "${jdpath}sendNotify.js"
+cat EthanWDC_jd_control/custom_sendNotify.js > "JDHelloWorld_jd_scripts_main/sendNotify.js"
 
 echo "update global parameters..."
-task "./update_global.js" now
+task "EthanWDC_jd_control/update_global.js" now
