7f4365cdb11866f120a5b2cff1ca9ff119b6a428
Ethan <64431009+EthanWDC@users.noreply.github.com>
2022-07-14 14:50:20 +0800

    Update update_global.js

--- update_global.js
+++ update_global.js
@@ -294,7 +294,7 @@ async function update_file(results) {
     try{
 	let cont_beansign = await fs.readFileSync(process.env.JDPATH + 'jd_bean_sign.ts', 'utf8');
  	//replace之后的内容稍有不同，防止重复replace
-    	cont_beansign = cont_beansign.replace(/replace\(\/红包\/g, \'红包\\n\\n\'\)/, "replace(/\\n【签到号一/g, '\\n\\n【签到号1').replace(/\\n【签到号二/g, '\\n\\n【签到号2').replace(/\\n【签到号三/g, '\\n\\n【签到号3').replace(/\\n【签到号四/g, '\\n\\n【签到号4').replace(/\\n【签到号五/g, '\\n\\n【签到号5').replace(/\\n【签到号六/g, '\\n\\n【签到号6').replace(/\\n【签到号七/g, '\\n\\n【签到号7').replace(/\\n【签到号八/g, '\\n\\n【签到号8').replace(/\\n【签到号九/g, '\\n\\n【签到号9').replace(/\\n【签到号十/g, '\\n\\n【签到号10')");
+    	cont_beansign = cont_beansign.replace(/replace\(\/红包\/g, \'红包\\n\\n\'\)/, "replace(/\\n【签到号/g, '\\n\\n【签到号 ').replace(/号 一/g, '号 1').replace(/号 二/g, '号 2').replace(/号 三/g, '号 3').replace(/号 四/g, '号 4').replace(/号 五/g, '号 5').replace(/号 六/g, '号 6').replace(/号 七/g, '号 7').replace(/号 八/g, '号 8').replace(/号 九/g, '号 9').replace(/号 十/g, '号 10')");
 	await fs.writeFileSync(process.env.JDPATH + 'jd_bean_sign.ts', cont_beansign, 'utf8');
     	console.log("jd_bean_sign.ts已成功修改!");
     }catch(e){
